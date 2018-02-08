8323193a8aa463a747ec1bf56110489a1fe2f442
Zaid Ajaj <Zaid-Ajaj@users.noreply.github.com>
2018-02-08 15:18:54 +0100

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,10 @@
 import java.util.*;
 import java.lang.*;
 
-/** The entry point of the application */
+/** The entry point of the application 
+ * @author Luna-Elise Schernthaner - s4703928
+ * @author Zaid Ajaj - s4807561
+ */
 public class Program
 {
     public static void main(String[] args)
